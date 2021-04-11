c011c1becddfeff41b5c4b90e8ab1b5687dc5e10
Yuri Kaffer <yurikaffer@gmail.com>
2021-04-11 18:30:33 -0300

    View About

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,10 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -18,7 +22,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -26,4 +30,13 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
+	
+	public static Scene GetMainScene() {
+		return mainScene;
+	}
+	
+	public static void main (String[] args) {
+		launch(args);
+	}
+	
 }
